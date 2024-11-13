169b0400887dce36d7f1acb235dac8455d4c321f
Jayesh0706 <nalawadejayesh0706@gmail.com>
2024-11-14 00:17:05 +0530

    Adding database queries

---
+++ database.sql
@@ -0,0 +1,3 @@
+create database user_db;
+create table users (user_id int primary key auto_increment ,name varchar(50), username varchar(50), email varchar(50), password nvarchar(max))
+create table user_images (user_id int , image_url nvarchar(max))
